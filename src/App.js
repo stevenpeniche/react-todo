335b903ba9612768726f117dabcf04d326c42549
Steven Peniche <stevenmpeniche@gmail.com>
2017-11-22 08:01:02 -0500

    Delete Todo: complete

--- src/App.js
+++ src/App.js
@@ -33,12 +33,17 @@ class App extends Component {
     this.setState({ todos: todos });
   }
 
+	deleteTodo(index) {
+		const todos = this.state.todos.filter( (todo) => todo !== this.state.todos[index])
+		this.setState({ todos: todos })
+	}
+
   render() {
     return (
       <div className="App">
 				<ul>
 					{ this.state.todos.map( (todo, index) =>
-						<ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } />
+						<ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } deleteTodo={ () => this.deleteTodo(index) }/>
 					)}
 				</ul>
 				<form onSubmit={ (e) => this.handleSubmit(e) }>
